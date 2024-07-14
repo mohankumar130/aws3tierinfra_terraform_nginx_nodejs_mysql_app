83156521c48f01dd62022bc75bd17e87158c1950
mohankumar130 <mohankumar130@gmail.com>
2024-07-14 19:39:06 +0530

    push

--- igw.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "demo_igw" {
-    vpc_id = aws_vpc.vpc_demo.id
-    tags = {
-      Name = "${var.tag_name[2]}-${"igw"}"
-    }
-}
--- private_routest.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_route" "private_routes_nat" {
-    route_table_id = aws_route_table.priv_rt.id
-    network_interface_id = aws_instance.nat_server.primary_network_interface_id
-    destination_cidr_block = var.cidr_block["any_cidr"]
-}
